d01aa3ab48148190ce1b9e2fa9f2dc5bcfb3091b
willianssf <willian.silva552@gmail.com>
2021-08-11 10:24:36 -0300

    delete implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -9,7 +9,7 @@ public interface SellerDao {
 	
 	void insert(Seller obj);
 	void upDate(Seller obj);
-	void deleteById(Seller obj);
+	void deleteById(Integer id);
 	Seller findById(Integer id);
 	List<Seller> findAll();
 	List<Seller> findByDepartment(Department department);
